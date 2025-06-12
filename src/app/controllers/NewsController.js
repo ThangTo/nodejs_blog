7481e35da2a1f69c042569c1a0e0d1637bf36bfb
To050 <149853520+To050@users.noreply.github.com>
2025-06-12 14:52:18 +0700

    Use MVC to reconfig, and install prettier, lint-staged, husky

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,14 +1,13 @@
 class NewsController {
+  // [GET] /news
+  index(req, res) {
+    res.render('news');
+  }
 
-    // [GET] /news
-    index(req, res){
-        res.render('news')
-    }
-
-    //[GET] /news/:slug
-    show(req, res){
-        res.send('NEW PAGE')
-    }
+  //[GET] /news/:slug
+  show(req, res) {
+    res.send('NEW PAGE');
+  }
 }
 
-module.exports = new NewsController;
+module.exports = new NewsController();
--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 const path = require('path');
-const express = require('express')
-const morgan = require('morgan')
-const { engine } = require("express-handlebars");
-const app = express()
-const port = 3000
-const route = require('./routes')
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const app = express();
+const port = 3000;
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded())
-app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
+app.use(express.json());
 
 //Http logger
 // app.use(morgan('combined'))
 
 //Template engine
-app.engine("hbs", engine({extname: '.hbs'}));
+app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on link http://localhost:${port}/`)
-})
+  console.log(`Example app listening on link http://localhost:${port}/`);
+});
