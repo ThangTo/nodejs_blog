d60657eb404b96698d18c76b1a833924c5bda2a4
ThangTo <your_email@example.com>
2025-07-15 09:40:13 +0700

    CRUD: read from DB, create new course, update course

--- src/index.js
+++ src/index.js
@@ -2,23 +2,43 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
+const methodOverride = require('method-override');
 const app = express();
 const port = 3000;
 const route = require('./routes');
 
+const db = require('./config/db');
+
+//Connect to MongoDB
+db.connect();
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
-//Http logger
-// app.use(morgan('combined'))
-
-//Template engine
-app.engine('hbs', engine({ extname: '.hbs' }));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.use(methodOverride('_method'));
 
 route(app);
+
+//Http logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine(
+  'hbs',
+  engine({
+    extname: '.hbs',
+    helpers: {
+      sum: (a, b) => a + b,
+    },
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 app.listen(port, () => {
   console.log(`Example app listening on link http://localhost:${port}/`);
---
+++ src/routes/courses.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const coursesController = require('../app/controllers/CourseController');
+
+router.post('/store', coursesController.store);
+router.get('/create', coursesController.create);
+router.get('/:id/edit', coursesController.edit);
+router.put('/:id', coursesController.update);
+router.get('/:slug', coursesController.show);
+
+module.exports = router;
